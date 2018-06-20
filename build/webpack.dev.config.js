78540681e3e393ad3a3bac00e0a371a4f258b345
dwj <dwi@163.com>
2018-06-20 23:35:58 +0800

    webpack 1.3

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -29,46 +29,49 @@ module.exports = {
     publicPath: '/'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
-        plugins: ['transform-runtime', 'add-module-exports'],
-        cacheDirectory: true
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react', 'stage-0', 'react-hmre'],
+          plugins: ['transform-runtime', 'add-module-exports'],
+          cacheDirectory: true
+        }
+      }, {
+        test: /\.scss$/,
+        loaders: [
+          'style',
+          'css?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:8]',
+          'sass'
+        ]
+      }, {
+        test: /\.(jpg|png|gif|webp)$/,
+        loader: 'url?limit=8000'
+      }, {
+        test: /\.json$/,
+        loader: 'json'
+      }, {
+        test: /\.html$/,
+        loader: 'html?minimize=false'
       }
-    }, {
-      test: /\.scss$/,
-      loaders: [
-        'style',
-        'css?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:8]',
-        'sass'
-      ]
-    }, {
-      test: /\.(jpg|png|gif|webp)$/,
-      loader: 'url?limit=8000'
-    }, {
-      test: /\.json$/,
-      loader: 'json'
-    }, {
-      test: /\.html$/,
-      loader: 'html?minimize=false'
-    }]
+    ]
   },
-  resolve: { extensions: ['', '.js', '.json', '.scss'] },
+  resolve: {extensions: ['', '.js', '.json', '.scss']},
   plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
       names: ['vendor', 'manifest'],
       filename: '[name].js'
     }),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV) }),
+    new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)}),
     new HtmlWebpackPlugin({
       filename: '../views/dev/index.html',
       template: './views/tpl/index.tpl.html'
     }),
-    new ProgressBarPlugin({ summary: false })
+    new ProgressBarPlugin({summary: false})
   ]
 }
