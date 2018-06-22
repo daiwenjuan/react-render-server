5a73d33e3a5c3ecac71c02a245d02fb508f057dd
daiwenjuan <daiwenjuan668@163.com>
2018-06-22 18:33:34 +0800

    client 入口文件

--- client/home/containers/App.js
+++ client/home/containers/App.js
@@ -2,41 +2,20 @@
  *  Created by daiwenjuan on 2018/6/20 下午2:47.
  */
 import React, { Component } from 'react'
-import { Upload, Icon, message } from 'antd'
+import { Upload, Icon, message, Input } from 'antd'
 
 const Dragger = Upload.Dragger
 
 export default class App extends Component {
-  constructor () {
+  constructor() {
     super()
   }
 
-  render () {
-    const props = {
-      name: 'file',
-      multiple: true,
-      action: '//jsonplaceholder.typicode.com/posts/',
-      onChange (info) {
-        const status = info.file.status
-        if (status !== 'uploading') {
-          console.log(info.file, info.fileList)
-        }
-        if (status === 'done') {
-          message.success(`${info.file.name} file uploaded successfully.`)
-        } else if (status === 'error') {
-          message.error(`${info.file.name} file upload failed.`)
-        }
-      },
-    }
+  render() {
     return (
-      <Dragger {...props}>
-        <p className="ant-upload-drag-icon">
-          <Icon type="inbox"/>
-        </p>
-        <p className="ant-upload-text">Click or drag file to this area to upload</p>
-        <p className="ant-upload-hint">Support for a single or bulk upload. Strictly prohibit from uploading company
-          data or other band files</p>
-      </Dragger>
+      <div>
+        <Input/>
+      </div>
     )
   }
 }
